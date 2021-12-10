c4f1fd0cb04a3ae6a71e333ffcce435f51b3ff1f
Tiago Martins <heimdhall@gmail.com>
2021-12-10 03:46:56 -0300

    data adicionada a consulta

--- postgresql-analysis/verificando quantidade de amostras por veiculo.sql
+++ postgresql-analysis/verificando quantidade de amostras por veiculo.sql
@@ -2,30 +2,27 @@ DO
 LANGUAGE 'plpgsql'
 $$
 DECLARE
-	texto_teste TEXT;
-	double_teste DOUBLE PRECISION;
-	integer_teste INTEGER;
-	record_teste RECORD;
-	counter INTEGER;
-	dates_text TEXT[];
+  dates DATE[];
 	i INTEGER;
 	table_name TEXT;
 	queries TEXT[];
   final_query TEXT;
+  date_text TEXT;
 BEGIN
 	-- Dropping table:
 	DROP TABLE IF EXISTS temp_veiculos_statistics;
 	-- Creating list of date values and putting into dates_text
-	SELECT array_agg(to_char(dates, 'YYYY_MM_DD')) INTO dates_text
-	FROM generate_series('2019-05-01','2019-05-14', '1 day'::INTERVAL) t(dates);
+	SELECT array_agg(d::DATE) INTO dates
+	FROM generate_series('2019-05-01','2019-05-14', '1 day'::INTERVAL) t(d);
   -- Looping through each date
-	FOR i IN 1 .. array_upper(dates_text, 1) LOOP
-		table_name := 'veiculos_'||dates_text[i]||'_bus_line_216';
+	FOR i IN 1 .. array_upper(dates, 1) LOOP
+	  date_text := to_char(dates[i],'YYYY_MM_DD');
+		table_name := 'veiculos_'||date_text||'_bus_line_216';
 		queries[i] := format('
-SELECT %L table_name, vehicle_id, COUNT(*) 
+SELECT %L table_name, vehicle_id, COUNT(*), %L::DATE file_date
 FROM %I 
 WHERE bus_line_id = %L 
-GROUP BY CUBE (vehicle_id)', table_name, table_name, '216');
+GROUP BY CUBE (vehicle_id)', table_name, dates[i], table_name, '216');
 	END LOOP;
   final_query := 'CREATE TEMP TABLE temp_veiculos_statistics AS (' || array_to_string(queries, ' UNION ALL ') || ');';
  EXECUTE final_query;
